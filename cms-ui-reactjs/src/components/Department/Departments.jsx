d8b791de55e81bb55ef2eee030b1cbb8e3b3ed31
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-09-30 12:35:07 +0530

    Forwarded proxy Section integration in to multiple components

--- cms-ui-reactjs/src/components/Department/Departments.jsx
+++ cms-ui-reactjs/src/components/Department/Departments.jsx
@@ -1,15 +1,15 @@
+import Section from "../Section.jsx";
 import { departments } from "./department.js";
 import Department from "./Department.jsx";
 
 export default function Departments() {
   return (
-    <section id="departments">
-      <h2>Departments</h2>
+    <Section title="Departments" id="departments">
       <ul>
         {departments.map((department) => (
           <Department key={department.title} {...department} />
         ))}
       </ul>
-    </section>
+    </Section>
   );
 }
