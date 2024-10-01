4ead3eb477db61a3add7fd382a0624e5cb4f810f
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-10-01 16:03:57 +0530

    Identified unnecessary state (activePlayer) to reduce state management  and reused existing gameTurns for the same.

--- cms-ui-reactjs/src/components/Tic-Tac-Toe/Game.jsx
+++ cms-ui-reactjs/src/components/Tic-Tac-Toe/Game.jsx
@@ -6,19 +6,26 @@ import Log from "./Log.jsx";
 
 import "./Game.css";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+}
 export default function Game() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  //const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
+    /**
     setActivePlayer((currentActivePlayer) =>
       currentActivePlayer === "X" ? "O" : "X"
     );
+    */
     setGameTurns((previousTurns) => {
-      let currentPlayer = "X";
-      if (previousTurns.length > 0 && previousTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      let currentPlayer = deriveActivePlayer(previousTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...previousTurns,
