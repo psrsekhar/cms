c9b48b10f8d75c77c1ea983a2d55582413f72cbf
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-10-02 01:03:52 +0530

    Added Input component to input data from user

--- cms-ui-reactjs/src/components/Investment-Calculator/Calculator.jsx
+++ cms-ui-reactjs/src/components/Investment-Calculator/Calculator.jsx
@@ -1,7 +1,13 @@
 import Header from "./Header.jsx";
+import Input from "./Input.jsx";
 
 import "./Calculator.css";
 
 export default function Calculator() {
-  return <Header></Header>;
+  return (
+    <section className="calculator">
+      <Header></Header>
+      <Input></Input>
+    </section>
+  );
 }
